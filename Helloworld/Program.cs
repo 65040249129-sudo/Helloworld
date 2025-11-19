be28cf9651f7d9a98db97f628b85472fcb3fefe9
Asus <Asus@LAPTOP-SB4J90IH>
2025-11-19 09:30:12 +0700

    Add project files.

---
+++ Helloworld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.WriteLine("My name is Suphakon Sikaeonamsai");
+            Console.WriteLine("computer science");
+            Console.WriteLine("-------------****-------------");
+            Console.WriteLine("My Hometown is ");
+            Console.WriteLine("Udonthani");
+            Console.WriteLine("Thailand");
+        }
+    }
+}
